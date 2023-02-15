ffbaea3719526ee2ee52686d80f06b10a4977d5a
cjohnson5048 <125328982+cjohnson5048@users.noreply.github.com>
2023-02-15 18:45:14 -0500

    Create repo1.sh
    Basic test script using if/then

---
+++ repo1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Setting variable
+read -p "What class are you in now: " class
+
+#Test script from online repo
+if [ class = Linux ]
+then
+  echo "Welcome to tonight's class. We are covering GITHUB."
+fi
